2d1f0c2b92b275640ed98406398461814d2b2830
Gabriel Delgado <gabrieldelgado37@gmail.com>
2021-06-15 00:54:14 -0300

    Alguns exercicios de logica utilizando .NET parte 3#

---
+++ InverterString/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace InverterString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string n = Console.ReadLine();
+            //continue a solução
+           
+            string textoInvertido = new string(n.Reverse().ToArray());
+            Console.WriteLine(textoInvertido);
+        }
+    }
+}
---
+++ TempodeumEvento/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace TempodeumEvento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int diaInicio, diaTermino, horaMomentoInicio, minutoMomentoInicio, segundoMomentoInicio;
+            int horaMomentoTermino, minutoMomentoTermino, segundoMomentoTerminio;
+
+            string[] dadosInicio = Console.ReadLine().Split();
+            diaInicio = Convert.ToInt32(dadosInicio[1]);
+
+            string[] dadosMomentoInicio = Console.ReadLine().Split();
+            horaMomentoInicio = Convert.ToInt32(dadosMomentoInicio[0]);
+            minutoMomentoInicio = Convert.ToInt32(dadosMomentoInicio[2]);
+            segundoMomentoInicio = Convert.ToInt32(dadosMomentoInicio[4]);
+
+            string[] dadosTermino = Console.ReadLine().Split();
+            diaTermino = Convert.ToInt32(dadosTermino[1]);
+
+            string[] dadosMomentoTermino = Console.ReadLine().Split();
+            horaMomentoTermino = Convert.ToInt32(dadosMomentoTermino[0]);
+            minutoMomentoTermino = Convert.ToInt32(dadosMomentoTermino[2]);
+            segundoMomentoTerminio = Convert.ToInt32(dadosMomentoTermino[4]);
+
+
+            int transformaSegundosInicio = ((diaInicio * 86400) + (horaMomentoInicio * 3600) + (minutoMomentoInicio * 60) + segundoMomentoInicio);
+            int transformaSegundosTermino = ((diaTermino * 86400) + (horaMomentoTermino * 3600) + (minutoMomentoTermino * 60) + segundoMomentoTerminio);
+
+            int somaTotalSegundos = (transformaSegundosTermino - transformaSegundosInicio);
+
+            int auxhora = (int)somaTotalSegundos / 3600;
+            somaTotalSegundos %= 3600;
+
+            int W = auxhora / 24;
+            int H = auxhora % 24;
+
+            int M = somaTotalSegundos / 60;
+            somaTotalSegundos %= 60;
+
+            int S = somaTotalSegundos;
+
+
+            Console.WriteLine("{0} dia(s)", W);
+            Console.WriteLine("{0} hora(s)", H);
+            Console.WriteLine("{0} minuto(s)", M);
+            Console.WriteLine("{0} segundo(s)", S);
+        }
+    }
+}
